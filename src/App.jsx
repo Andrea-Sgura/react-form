43b94e01afe3484ee07e8e5f920a191689a6244c
Andrea-Sgura <andreasgura96@gmail.com>
2025-05-19 00:47:40 +0200

    [UPD] Cancellato file app.css, pulito file index.css, inserito bootstrap, inserito array in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const posts = [
+  {
+    id: 1,
+    title: "Post 1",
+  },
+  {
+    id: 2,
+    title: "Post 2",
+  },
+  {
+    id: 3,
+    title: "Post 3",
+  },
+  {
+    id: 4,
+    title: "Post 4",
+  },
+  {
+    id: 5,
+    title: "Post 5",
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div></div>
   )
 }
 
